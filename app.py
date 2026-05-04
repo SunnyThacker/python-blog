582d44f5d041ccf93be14fc729c481940a3701a2
SunnyThacker <sunny.thackar@gmail.com>
2026-05-04 16:27:05 +0530

    revert

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 posts = [
     {
         'id': 1,
-        'title': 'Welcome to My Blog Sunny',
+        'title': 'Welcome to My Blog',
         'content': 'This is the first blog post. Welcome to our blogging platform!',
         'author': 'Admin',
         'date': '2026-05-01'
